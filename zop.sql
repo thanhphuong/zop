2dd087f085c908e75f04857d253f514091ccb557
Bui Thanh Phuong <btphuong@acrobits.cz>
2012-10-07 23:57:17 +0700

    database init

---
+++ zop.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Ultimate v8.61 
+MySQL - 5.5.25a : Database - zop
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`zop` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `zop`;
+
+/*Table structure for table `zop_account` */
+
+DROP TABLE IF EXISTS `zop_account`;
+
+CREATE TABLE `zop_account` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Email` varchar(200) NOT NULL,
+  `Phone` varchar(30) DEFAULT NULL,
+  `Password` varchar(30) DEFAULT NULL,
+  `Name` varchar(30) NOT NULL,
+  `First_Name` varchar(15) NOT NULL,
+  `Last_Name` varchar(15) NOT NULL,
+  `Gender` int(1) NOT NULL,
+  `Birth_Day` date NOT NULL,
+  `Avatar` varchar(30) DEFAULT NULL,
+  `Created_Date` datetime NOT NULL,
+  `Updated_Date` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `zop_account` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
