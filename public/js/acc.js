93b1abb6210c7d501c94af893244a62c7ef31a65
Phuong Bui <btphuong@acrobits.cz>
2012-10-08 17:25:12 +0700

    selectYear function

--- public/js/acc.js
+++ public/js/acc.js
@@ -1,55 +1,88 @@
-function selectMonth() {
+function selectYear(){
 	var day = document.getElementById("birthday_day");
 	var month = document.getElementById("birthday_month");
 	var year = document.getElementById("birthday_year");
 
-	// var valueDay = day.options[day.selectedIndex].value;
-	var valueMonth = month.options[month.selectedIndex].value;
-	// var valueYear = year.options[year.selectedIndex].value;
+	if (month.selectedIndex != 2)
+		return;
+	
+	if (year.selectedIndex == 0){		
+		changeOptionDay(day, 29);
+	}else{
+		var valueYear = parseInt(year.options[year.selectedIndex].value);						
+		if ((valueYear % 4 == 0 && valueYear % 100 != 0) || valueYear % 400 == 0){			
+			changeOptionDay(day, 29);
+		}else{
+			changeOptionDay(day, 28);
+		}
+	}
+	
+}
+function selectMonth() {
+	var day = document.getElementById("birthday_day");
+	var month = document.getElementById("birthday_month");
+	var year = document.getElementById("birthday_year");	
+	
+	var valueMonth = month.options[month.selectedIndex].value;	
 
 	switch (parseInt(valueMonth)) {
 	case 1:
 		changeOptionDay(day, 31);
 		break;
 	case 2:
-		changeOptionDay(day, 29);
+		if (year.selectedIndex == 0){
+			changeOptionDay(day, 29);
+		}else{
+			var valueYear = parseInt(year.options[year.selectedIndex].value);
+			if (valueYear % 4 == 0 && valueYear % 100 != 0){
+				changeOptionDay(day, 29);
+			}else{
+				changeOptionDay(day, 28);
+			}
+		}
 		break;
 	case 3:
-
+		changeOptionDay(day, 31);
 		break;
 	case 4:
-
+		changeOptionDay(day, 30);
 		break;
 	case 5:
-
+		changeOptionDay(day, 31);
 		break;
 	case 6:
-
+		changeOptionDay(day, 30);
 		break;
 	case 7:
-
+		changeOptionDay(day, 31);
 		break;
 	case 8:
-
+		changeOptionDay(day, 31);
 		break;
 	case 9:
-
+		changeOptionDay(day, 30);
 		break;
 	case 10:
-
+		changeOptionDay(day, 31);
 		break;
 	case 11:
-
+		changeOptionDay(day, 30);
 		break;
 	case 12:
-
+		changeOptionDay(day, 31);
 		break;
 	default:
+		changeOptionDay(day, 31);
 		break;
 	}
 }
 
 function changeOptionDay(day, n) {
+	var indexDay = day.selectedIndex;
+	if (indexDay > n){
+		day.options[0].selected = true;
+	}
+	
 	var length = day.length;
 	if (length == n + 1)
 		return;
@@ -69,10 +102,5 @@ function changeOptionDay(day, n) {
 			day.remove(length - 1 - i);			
 		}
 	}
-	
-//	if (myselect.options[i].selected==true){
-//		  alert("Selected Option's index: "+i)
-//		  break
-//		 }
 
 }
