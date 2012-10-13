9f8fd04a946c2d29356f412bbc5c6e7aa1409ea5
Bui Thanh Phuong <btphuong@acrobits.cz>
2012-10-14 01:12:07 +0700

    zend library

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,7 @@ return array(
     'modules' => array(
         'Application',
     		'Account',
+    		'Common',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
